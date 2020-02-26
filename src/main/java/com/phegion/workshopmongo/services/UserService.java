bb1d97f6530e94d2a2f7e63702440dc679bc3f48
Phegion <marcoantoniocastilholeite@gmail.com>
2020-02-26 08:56:28 -0300

    Conectando ao MongoDB com repository e service

---
+++ src/main/java/com/phegion/workshopmongo/services/UserService.java
@@ -0,0 +1,22 @@
+package com.phegion.workshopmongo.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.phegion.workshopmongo.domain.User;
+import com.phegion.workshopmongo.repository.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repo;
+	
+	public List<User> findAll(){
+		return repo.findAll();
+	}
+		
+	
+}
