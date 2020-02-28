6eb6ceb2833de0b7ffc8ff6e3d2def62f02d76cd
Phegion <marcoantoniocastilholeite@gmail.com>
2020-02-28 08:47:31 -0300

    Obtendo um post por id

---
+++ src/main/java/com/phegion/workshopmongo/services/PostService.java
@@ -0,0 +1,29 @@
+package com.phegion.workshopmongo.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.phegion.workshopmongo.domain.Post;
+import com.phegion.workshopmongo.domain.User;
+import com.phegion.workshopmongo.repository.PostRepository;
+import com.phegion.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+
+	public List<Post> findAll() {
+		return repo.findAll();
+	}
+
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+}
+	
