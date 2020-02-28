348b8ae351620079ef22711e752c4084d5e14337
Phegion <marcoantoniocastilholeite@gmail.com>
2020-02-28 08:21:02 -0300

    Projeção dos dados do autor com DTO

--- src/main/java/com/phegion/workshopmongo/config/Instantiation.java
+++ src/main/java/com/phegion/workshopmongo/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.phegion.workshopmongo.domain.Post;
 import com.phegion.workshopmongo.domain.User;
+import com.phegion.workshopmongo.dto.AuthorDTO;
 import com.phegion.workshopmongo.repository.PostRepository;
 import com.phegion.workshopmongo.repository.UserRepository;
 
@@ -17,34 +18,30 @@ import com.phegion.workshopmongo.repository.UserRepository;
 public class Instantiation implements CommandLineRunner {
 
 	@Autowired
-	private UserRepository userRepository;
-	
+	private UserRepository userReposiroty;
+
 	@Autowired
-	private PostRepository postRepository;
-	
+	private PostRepository postReposiroty;
+
 	@Override
-	public void run(String... args) throws Exception {
+	public void run(String... arg0) throws Exception {
 		
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
 		
-		userRepository.deleteAll();
-		postRepository.deleteAll();
+		userReposiroty.deleteAll();
+		postReposiroty.deleteAll();
 		
 		User maria = new User(null, "Maria Brown", "maria@gmail.com");
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
+		
+		userReposiroty.saveAll(Arrays.asList(maria, alex, bob));
 
-		Post post1 = new Post(null, sdf.parse("21/03/2019"), "Partiu viajem", "Vou viajar para SP, abraços", maria);
-		Post post2 = new Post(null, sdf.parse("23/03/2019"), "Bom dia", "Acordei feliz hoje", maria);
-		
-		userRepository.saveAll(Arrays.asList(maria, alex, bob));
-		postRepository.saveAll(Arrays.asList(post1, post2));
-		
-		
-		
-		
-		
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+
+		postReposiroty.saveAll(Arrays.asList(post1, post2));
 	}
 
-}
+}	
